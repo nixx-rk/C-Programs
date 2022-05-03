668a0c1b8fa63b89d50681e7c9bc23a7bbf70ecb
NIKHIL R KUMAR <90245190+nixx-rk@users.noreply.github.com>
2022-05-04 00:29:43 +0530

    Add files via upload

---
+++ SimpleInterest.c
@@ -0,0 +1,21 @@
+//Calclation of Simple Interest 
+
+#include<stdio.h>
+
+void main()
+{
+    int p,n;
+    float r,si;
+
+    printf("\nEnter the Principle amount\n");
+    scanf("%d",&p);
+    printf("\nEnter the number of years of period\n");
+    scanf("%d",&n);
+    printf("\nEnter the rate of interest\n");
+    scanf("%f",&r);
+
+    /*Formula for Simple Interest*/
+    si=p*n*r/100;
+    printf("\nSimple Interest: %.2f\n",si);
+
+}
